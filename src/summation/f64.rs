05e72be14d61abc591b99d4ea0a610484dcaec2d
Heeten <heeten.choxi@gmail.com>
2023-04-13 10:06:49 -0400

    Repo at end of unit test chapter

--- src/summation/f64.rs
+++ src/summation/f64.rs
@@ -1,3 +1,14 @@
 pub fn summation_f64(values: &[f64]) -> f64 {
     values.iter().sum()
 }
+
+#[cfg(test)]
+mod test {
+    use super::summation_f64;
+
+    #[test]
+    fn simple_test() {
+        let res = summation_f64(&[0.0, 1.0,  2.0]);
+        assert_eq!(res, 3.0);
+    }
+}
