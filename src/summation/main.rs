0f403dccdf735337a0d44a4ce96d82e4857f0e4b
Heeten <heeten.choxi@gmail.com>
2023-04-14 09:24:49 -0400

    Use clap in binary

--- src/summation/main.rs
+++ src/summation/main.rs
@@ -1,7 +1,29 @@
+use clap::{Parser, Subcommand};
 use src_summation::f64::summation_f64;
-use std::env;
+use src_summation::u32::summation_u32;
+
+#[derive(Subcommand)]
+enum Cmd {
+    U32 { args: Vec<String> },
+    F64 { args: Vec<String> },
+}
+
+#[derive(Parser)]
+struct Arguments {
+    #[command(subcommand)]
+    cmd: Cmd,
+}
 
 fn main() {
-    let args: Vec<f64> = env::args().skip(1).map(|a| a.parse().unwrap()).collect();
-    println!("sum = {}", summation_f64(&args))
+    let args = Arguments::parse();
+    match args.cmd {
+        Cmd::U32 { args } => {
+            let args: Vec<u32> = args.into_iter().map(|a| a.parse().unwrap()).collect();
+            println!("sum = {}", summation_u32(&args))
+        }
+        Cmd::F64 { args } => {
+            let args: Vec<f64> = args.into_iter().map(|a| a.parse().unwrap()).collect();
+            println!("sum = {}", summation_f64(&args))
+        }
+    }
 }
